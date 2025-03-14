17c3a7958fe6a6e5d1ded92465c0ce4d336da1de
yuminnnnni <ka09023@ajou.ac.kr>
2025-03-14 16:20:17 +0900

    [fix] : Google Analytics 페이지 트래킹 개선 (#244)

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,6 @@ export const App = () => {
   useEffect(() => {
     // 첫번째 경로 세그먼트만 추출
     const trackPath = '/' + location.pathname.split('/')[1];
-
-    console.log('Track Path', trackPath);
     trackPageView(trackPath);
   }, [location.pathname]);
 
